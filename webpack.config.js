11e5dcd356c68b6b062865d24cddcdbdb9cce071
Borisstoy <boris.stoy@gmail.com>
2017-08-30 13:42:00 -0400

    :bug: Fix package.json dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -14,16 +14,6 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }],
-    rules: [{
-      test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader" // compiles Sass to CSS
-      }]
     }]
   },
   resolve: {
