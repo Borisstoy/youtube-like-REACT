7a54e8d60184060a18dc6576054ae4b7e49bc894
Borisstoy <boris.stoy@gmail.com>
2017-08-30 15:12:52 -0400

    :wrench: Test jsx engine

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+// set the view engine to jsx
+app.set('view engine', 'jsx');
+
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
