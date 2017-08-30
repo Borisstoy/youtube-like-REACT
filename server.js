1a3af48bf3eb37de6217f2a22d8d4a98b82b99e3
Borisstoy <boris.stoy@gmail.com>
2017-08-30 15:05:15 -0400

    :wrench: Switch from ejs to js view engine

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+// set the view engine to js
+app.set('view engine', 'js');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
