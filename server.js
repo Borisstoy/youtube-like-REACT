dd0079e7516cc3c01794a9808c3e24f865100110
Borisstoy <boris.stoy@gmail.com>
2017-08-30 15:28:52 -0400

    :wrench: Change procfile pointer

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to jsx
-app.set('view engine', 'jsx');
+app.set('view engine', 'js');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
