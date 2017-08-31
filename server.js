7d6cfa515852f69c2e20e0af97e1465c4e0b8994
Borisstoy <boris.stoy@gmail.com>
2017-08-31 12:32:23 -0400

    :wrench: Add newrelic

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+require('newrelic');
+
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
